0847afed510458f732718137ed8791450005ee8b
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-04-29 17:01:33 +0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,18 @@
+import time
+
+class timer:
+	def start():
+		h = 0
+		m = 0
+		s = 0
+		while True:
+			s += 1
+			if s >= 60:
+				m += 1
+				s = 0
+			if m > 60:
+				h += 1
+				m = 0
+				s = 0
+			time.sleep(1)
+			print('{}:{}:{}'.format(h, m, s))
